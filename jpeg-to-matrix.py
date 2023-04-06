079cf67b107101ce5c9b3160d142e6683be5b842
Anton <whafu@kth.se>
2023-04-06 14:37:52 +0200

    Convert `jpeg` to grayscale arrays

---
+++ jpeg-to-matrix.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+import sys
+from PIL import Image 
+import numpy as np
+import os
+
+
+def main():
+    source_dir = Path('images/train/')
+    files = source_dir.iterdir()
+    for file in files:
+        img = Image.open(file).convert('L')
+        img = np.array(img)
+
+        np.save(f'images/mat_files/{os.path.basename(file)}', img)
+
+if __name__ == "__main__":
+    main()    
